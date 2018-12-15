5e61818fef192c49517bf28be9e66f4dd0fe8eae
李新锐 <hongwen000@outlook.com>
2018-12-15 23:34:24 +0800

    implement query_user

--- code/app/app.js
+++ code/app/app.js
@@ -34,5 +34,52 @@ app.all('/api/query', function (req, res) {
     });
 });
 app.use('/static', express.static(path.join(__dirname, 'static/')));
+app.get('/api/query_user', function (req, res) {
+    var credential = req.body.credential;
+    var name = req.body.name;
+    var gender = req.body.number;
+    var phone = req.body.phone;
+    var balance_min = req.body.balance_min;
+    var balance_max = req.body.balance_max;
+    var bonus_min = req.body.bonus_min;
+    var bonus_max = req.body.bonus_max;
+    var query = 'select * from User as u where 1 = 1';
+    // 精确匹配证件号
+    if (credential != '') {
+        query.concat(' and u.credential = ' + credential);
+    }
+    // 模糊匹配姓名
+    if (name != '') {
+        query.concat(" and u.name LIKE'%" + name + "%'");
+    }
+    if (gender != -1) {
+        query.concat(" and u.gender = " + gender.toString());
+    }
+    if (phone != '') {
+        query.concat(' and u.phone = ' + phone);
+    }
+    if (balance_min != -1) {
+        query.concat(" and u.balance >= " + balance_min.toString());
+    }
+    if (balance_max != -1) {
+        query.concat(" and u.balance <= " + balance_max.toString());
+    }
+    if (bonus_min != -1) {
+        query.concat(" and u.bonus >= " + bonus_min.toString());
+    }
+    if (bonus_max != -1) {
+        query.concat(" and u.bonus <= " + bonus_max.toString());
+    }
+    pool.getConnection()
+        .then(function (conn) {
+        conn.query(query)
+            .then(function (table) {
+            res.json({
+                'data': JSON.stringify(table)
+            });
+            console.log(table);
+        });
+    });
+});
 app.listen(port, function () { return console.log("Example app listening on port " + port + "!"); });
 exports["default"] = app;
--- code/app/app.ts
+++ code/app/app.ts
@@ -38,6 +38,55 @@ app.all('/api/query', (req: Request, res: Response) => {
 });
 
 app.use('/static', express.static(path.join(__dirname, 'static/')));
+
+app.get('/api/query_user', (req: Request, res: Response)=>{
+  let credential: string =  req.body.credential;
+  let name: string = req.body.name;
+  let gender: number = req.body.number;
+  let phone: string = req.body.phone;
+  let balance_min: number = req.body.balance_min;
+  let balance_max: number = req.body.balance_max;
+  let bonus_min: number = req.body.bonus_min;
+  let bonus_max: number = req.body.bonus_max;
+  let query: string = 'select * from User as u where 1 = 1';
+  // 精确匹配证件号
+  if(credential != '') {
+    query.concat(' and u.credential = ' + credential);
+  }
+  // 模糊匹配姓名
+  if(name != '') {
+    query.concat(" and u.name LIKE'%"+ name +"%'")
+  }
+  if(gender != -1) {
+    query.concat(" and u.gender = " + gender.toString());
+  }
+  if(phone != '') {
+    query.concat(' and u.phone = ' + phone);
+  }
+  if(balance_min != -1) {
+    query.concat(" and u.balance >= " + balance_min.toString());
+  }
+  if(balance_max != -1) {
+    query.concat(" and u.balance <= " + balance_max.toString());
+  }
+  if(bonus_min != -1) {
+    query.concat(" and u.bonus >= " + bonus_min.toString());
+  }
+  if(bonus_max != -1) {
+    query.concat(" and u.bonus <= " + bonus_max.toString());
+  }
+  pool.getConnection()
+    .then(conn=>{
+      conn.query(query)
+        .then((table)=>{
+          res.json({
+            'data': JSON.stringify(table)
+          })
+          console.log(table);
+        })
+    })
+
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
